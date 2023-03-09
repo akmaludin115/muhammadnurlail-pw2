12bd201435e68fc0defbb82edeccc7db32369231
akmaludin115 <azzamlail15@gmail.com>
2023-03-09 15:31:48 +0700

    upload praktik 3

---
+++ praktek3/proses_registrasi.php
@@ -0,0 +1,19 @@
+<?php
+
+$domisili = ["jakarta", "bogor", "depok", "tanggerang", "bekasi"];
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informasi",
+    "BD"=>"Bisnis Digital"
+];
+
+$skill_user = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50,
+];
+?>
